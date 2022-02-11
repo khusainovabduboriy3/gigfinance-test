d5853005a8139456bd7a0119a2569c9e13cff005
khusainovabduboriy3 <96875761+khusainovabduboriy3@users.noreply.github.com>
2022-02-11 18:18:31 +0500

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,56 @@
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+  
+    if (scroll >= 10) {
+        $("nav").addClass("active");
+        $("header").addClass("active");
+        $(".toup").addClass("active");
+    }
+    else{
+        $("nav").removeClass("active");
+        $("header").removeClass("active");
+        $(".toup").removeClass("active");
+    }
+  });
+
+
+  $('.menu').click(function(){
+  $('.for-flex').toggleClass('active');
+  $('.line').toggleClass('active');
+  $('.menu2').toggleClass('opened');
+  });
+  $('.menu2').click(function(){
+    $('.menu').toggleClass('opened');
+    });
+
+
+
+  var typed = new Typed('.element', {
+    strings: $(".element").attr('data-elements').split(','),
+    typeSpeed: 80,
+    onStop: (_, self) => {
+      clearInterval(self.timeout)
+     },
+  });
+
+
+
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    nav:true,
+    autoplayTimeout:7000,
+    responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:2
+      },
+      900:{
+        items:4
+    }
+  }
+});
